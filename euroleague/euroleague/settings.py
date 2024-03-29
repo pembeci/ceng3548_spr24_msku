8ad1dc02c399c82cea96015be4fadb766864d066
Izzet Pembeci <izzetpembeci@vituscommodities.com>
2024-03-29 14:04:45 +0300

    settings.py: Enable our new pipeline

--- euroleague/euroleague/settings.py
+++ euroleague/euroleague/settings.py
@@ -25,7 +25,7 @@ CONCURRENT_REQUESTS = 12
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.2
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -62,9 +62,9 @@ DOWNLOAD_DELAY = 0.2
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "euroleague.pipelines.EuroleaguePipeline": 300,
-#}
+ITEM_PIPELINES = {
+    "euroleague.pipelines.EuroleaguePipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
